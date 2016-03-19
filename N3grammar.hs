88e8785f40c1f7a369d7e1e78fac50992c25c0eb
Dörthe Arndt <dorthe.arndt@ugent.be>
2016-03-19 22:00:41 +0100

    fixed anonymous blanks in implications

--- N3grammar.hs
+++ N3grammar.hs
@@ -10,13 +10,26 @@ import Text.Parsec.String
 import Text.Parsec.Expr
 import Text.Parsec.Token
 import Text.Parsec.Language
-
+import Control.Monad.Supply
 
 
 data S = Mainformula Formula deriving Show
-data Term = URI String | Universal String | Existential String | Literal String | Exp Expression | Objectlist Term Term | PredicateObjectList Term Term Term | BlankConstruct Term Term |Blank String deriving Show 
+data Term =  URI String 
+           | Universal String 
+           | Existential String 
+           | Literal String 
+           | Exp Expression 
+--TODO Exp2 construction should be fixed
+           | Exp2 Expression 
+           | Objectlist Term Term 
+           | PredicateObjectList Term Term Term 
+           | BlankConstruct Term Term 
+           | Blank String 
+           deriving Show 
 data Expression = FE Formula | BE Bool deriving Show
-data Formula = Triple Term Term Term | Conjunction Formula Formula | Implication Expression Expression  deriving Show 
+data Formula = Triple Term Term Term | Conjunction Formula Formula | Implication Expression Expression deriving Show 
+
+--TODO separate input grammer from pure grammar then translate to core
 
 def = emptyDef{ commentLine = "#"
               , opStart = oneOf "; ,.=><"
@@ -30,7 +43,7 @@ TokenParser{ identifier = m_identifier
            , reserved = m_reserved  
            } = makeTokenParser def
 
-
+--todo: need to make sure that the blanks from both sides are different
 formulaparser :: Parser Formula
 formulaparser = try (do {
                        f1 <- simpleformula
@@ -47,7 +60,7 @@ simpleformula =  try (do {
                       s <- termparser
                      ;p <- termparser
                      ;o <- objectparser
-                     ; return (translate s p o 1)
+                     ; return (Triple s p o)
                      })
                 <|> try(do {
                      e1 <- exparser
@@ -81,8 +94,10 @@ termparser = fmap URI (char ':' >> m_identifier)
       <|> fmap Universal (char '?' >> m_identifier)
       <|> fmap Existential (char '_' >> char ':' >> m_identifier)
       <|> fmap Literal ( m_identifier)
-      <|> fmap Exp ( exparser )
-      <|> try (do {m_reserved "[]"
+      <|> fmap Exp2 ( exparser )
+      <|> try (
+              do {
+              m_reserved "[]"
               ;return (Blank "blank")
              })
       <|> do { m_reserved "["
@@ -111,7 +126,7 @@ mainparser = mparser <* eof
                   mparser = do {
                                f <- formulaparser
                                ;m_reserved "."
-                               ; return (Mainformula f)
+                               ; return (Mainformula (cleanUp f "blank"))
                                
                                }
    
@@ -122,25 +137,79 @@ parseN3 inp = case parse mainparser "" inp of
               }
 
 
---todo: this works but has to be cleaned up---
-
-translate :: Term -> Term -> Term -> Int -> Formula
-translate (Blank "blank")      p o          n  =             (translate  (Blank ("blank_"++show(n) )) p o   (n+1) )
-translate s     (Blank "blank")  o          n  =             (translate  s (Blank ("blank_"++show(n) )) o   (n+1) )
-translate s     p (Blank "blank")           n  =             (translate  s p (Blank ("blank_"++show(n) ))   (n+1) )
-translate (BlankConstruct a b) p o          n  = Conjunction (translate  (Blank ("blank_"++show(n) )) p o   (n+1) )  
-                                                             (translate  (Blank ("blank_"++show(n) )) a b   (n+1) ) 
-translate s (BlankConstruct a b) o          n  = Conjunction (translate   s (Blank ("blank_"++show(n) )) o  (n+1) )  
-                                                             (translate  (Blank ("blank_"++show(n) )) a b   (n+1) ) 
-translate s p (BlankConstruct a b)          n  = Conjunction (translate   s p (Blank ("blank_"++show(n) ))  (n+1) )  
-                                                             (translate  (Blank ("blank_"++show(n) )) a b   (n+1) )   
-translate s p (Objectlist o list)           n  = Conjunction (translate s p o (n+1))  (translate s p  list   (n+1) )
-translate s p (PredicateObjectList o p2 o2) n  = Conjunction (translate s p o (n+1))  (translate s p2 o2     (n+1) )
-translate s p o                             n  = Triple s p o
+--todo: this works but has to be cleaned up (you can for sure do it shorter)---
 
 
-newBlankName :: String -> String -> String
-newBlankName a b = a ++ "_" ++ show(length(a)) ++"_" ++ b
+
+
+
+
+--function to get rid of blank node constructions in expressions
+cleanExp :: Expression -> String -> Expression
+cleanExp (FE f)  st = (FE (cleanUp f st ))
+cleanExp e       st = e
+
+
+cleanUp :: Formula -> String -> Formula
+cleanUp (Implication e1 e2) st = (Implication ( cleanExp e1 (st++show(1)))( cleanExp e2 (st++show(2))))
+cleanUp (Conjunction f1 f2) st = (Conjunction ( cleanUp  f1 (st++show(1)))( cleanUp  f2 (st++show(2))))
+
+cleanUp (Triple (Blank "blank") p o ) st = cleanUp (Triple ( Existential (st++show(1))) p o) (st++show(1))   
+cleanUp (Triple s (Blank "blank") o ) st = cleanUp (Triple  s (Existential (st++show(2))) o) (st++show(2)) 
+cleanUp (Triple s p (Blank "blank") ) st = cleanUp (Triple  s p (Existential (st++show(3)))) (st++show(3)) 
+
+cleanUp (Triple (Exp2 e) p o )        st  = cleanUp (Triple ( Exp (cleanExp e (st++show(1)))) p o) (st++show(1)) 
+cleanUp (Triple s (Exp2 e) o )        st  = cleanUp (Triple  s (Exp (cleanExp e (st++show(2)))) o) (st++show(2))
+cleanUp (Triple s p (Exp2 e) )        st  = cleanUp (Triple  s p (Exp (cleanExp e (st++show(3))))) (st++show(3))
+
+cleanUp (Triple (BlankConstruct a b) p o) st = (\x 
+                                                -> 
+                                               (cleanUp (Conjunction (Triple (Existential x) p o) (Triple (Existential x) a b)) x ))
+                                               (st++show(1))
+cleanUp (Triple s (BlankConstruct a b) o) st = (\x 
+                                               -> 
+                                               (cleanUp (Conjunction (Triple s (Existential x) o) (Triple (Existential x) a b)) x ))
+                                               (st++show(2))
+cleanUp (Triple s p (BlankConstruct a b)) st = (\x 
+                                               ->
+                                               (cleanUp (Conjunction (Triple s p (Existential x)) (Triple (Existential x) a b)) x ))
+                                               (st++show(3))
+
+cleanUp (Triple s p (Objectlist o list) ) st          = cleanUp (Conjunction (Triple s p o) (Triple s p list)) st
+cleanUp (Triple s p (PredicateObjectList o p2 o2)) st = cleanUp (Conjunction (Triple s p o) (Triple s p2 o2))  st 
+
+cleanUp f st = f
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
